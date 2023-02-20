9420d94a8746713739b8d029ab364fdb98c63720
asesidaa <1061472754@qq.com>
2023-02-20 15:42:50 +0800

    Initial commit

---
+++ LLServer/Controllers/BasicInfoController.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LLServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BasicInfoController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult<BasicInfo> GetBasicInfo()
+    {
+        var aes = Aes.Create();
+        aes.Mode = CipherMode.CBC;
+        aes.KeySize = 128;
+        aes.GenerateKey();
+        var key = BitConverter.ToString(aes.Key).Replace("-", "");
+
+        var info = new BasicInfo
+        {
+            Response = new BasicInfoResponse
+            {
+                BaseUrl = "http://127.0.0.1:1234",
+                DownloadUrl = "http://127.0.0.1:1234",
+                Key = key,
+                Iv = "0000000000000000",
+                TenpoIndex = 1
+            }
+        };
+
+        return Ok(info);
+    }
+}
---
+++ LLServer/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.HttpLogging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
