ef4772925cecb22f6fc84ac57c4e91a6395c3ff9
asesidaa <1061472754@qq.com>
2023-07-13 02:08:54 +0800

    Add missing nesys endpoints

---
+++ LLServer/Controllers/Nesys/CardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LLServer.Controllers.Nesys;
+
+[ApiController]
+[Route("service/card")]
+public class CardController : ControllerBase
+{
+    [HttpPost("cardn.cgi")]
+    public IActionResult Card()
+    {
+        return Ok("1\n1,1\n7020392010281502");
+    } 
+}
