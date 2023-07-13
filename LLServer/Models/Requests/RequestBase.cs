fae1291b8dcfa8bced398580af9022480597a1b6
BocuD <duco.born@gmail.com>
2023-07-13 06:19:13 +0200

    Add param to requestbase

--- LLServer/Models/Requests/RequestBase.cs
+++ LLServer/Models/Requests/RequestBase.cs
@@ -1,8 +1,10 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace LLServer.Models.Requests;
 
 public class RequestBase
 {
     [JsonPropertyName("protocol")] public string Protocol { get; set; } = string.Empty;
+    [JsonPropertyName("param")] public JsonElement? Param { get; set; }
 }
