a9652b2d8f978d4378e87a0506a47dfb23f34d63
BocuD <duco.born@gmail.com>
2023-07-18 16:59:54 +0200

    Null checking reflection based mapper for generic data

---
+++ LLServer/Mappers/ReflectionMapper.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace LLServer.Mappers;
+
+public class ReflectionMapper<TInput, TOutput>
+{
+    public TOutput Map(TInput input, TOutput target)
+    {
+        Type inputType = typeof(TInput);
+        Type targetType = typeof(TOutput);
+        
+        PropertyInfo[] inputProperties = inputType.GetProperties();
+        PropertyInfo[] outputProperties = targetType.GetProperties();
+        
+        foreach (PropertyInfo inputProperty in inputProperties)
+        {
+            PropertyInfo? outputProperty = outputProperties.FirstOrDefault(x => x.Name == inputProperty.Name);
+            if (outputProperty == null)
+            {
+                continue;
+            }
+            
+            //ignore property if it has the [JsonIgnore] or [Key] attribute
+            if (inputProperty.GetCustomAttribute<JsonIgnoreAttribute>() != null ||
+                inputProperty.GetCustomAttribute<KeyAttribute>() != null)
+            {
+                continue;
+            }
+
+            object? inputValue = inputProperty.GetValue(input);
+            if (inputValue == null)
+            {
+                continue;
+            }
+            outputProperty.SetValue(target, inputValue);
+        }
+
+        return target;
+    }
+}
