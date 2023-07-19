003ef6478ca4f81f68f7a9bc85b96d1c46e1e831
BocuD <duco.born@gmail.com>
2023-07-19 02:39:32 +0200

    Screen filter level db migration

---
+++ LLServer/Database/Migrations/20230719003900_Screen filter level.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LLServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Screenfilterlevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ScreenFilterLevel",
+                table: "UserData",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScreenFilterLevel",
+                table: "UserData");
+        }
+    }
+}
