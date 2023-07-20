927fa47fdcbc20772a2faff005ff5f833541e8e0
BocuD <duco.born@gmail.com>
2023-07-20 07:45:38 +0200

    Database migration for achievement record books

---
+++ LLServer/Database/Migrations/20230720054525_Achievement record book.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LLServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Achievementrecordbook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AchievementRecordBooks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    Values = table.Column<string>(type: "TEXT", nullable: false),
+                    UserID = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AchievementRecordBooks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AchievementRecordBooks_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AchievementRecordBooks_UserID",
+                table: "AchievementRecordBooks",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AchievementRecordBooks");
+        }
+    }
+}
