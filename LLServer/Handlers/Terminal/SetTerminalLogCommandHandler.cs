45535baa61b2c55222dd0645ef30e66d193ef840
BocuD <duco.born@gmail.com>
2023-07-29 15:31:06 +0200

    Create TerminalLogHandler

---
+++ LLServer/Handlers/Terminal/SetTerminalLogCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using LLServer.Common;
+using LLServer.Models.Requests;
+using LLServer.Models.Responses;
+using MediatR;
+
+namespace LLServer.Handlers.Terminal;
+
+public record SetTerminalLogCommand(RequestBase request) : IRequest<ResponseContainer>;
+
+public class SetTerminalLogCommandHandler : IRequestHandler<SetTerminalLogCommand, ResponseContainer>
+{
+    public async Task<ResponseContainer> Handle(SetTerminalLogCommand request, CancellationToken cancellationToken)
+    {
+        return StaticResponses.EmptyResponse;
+    }
+}
