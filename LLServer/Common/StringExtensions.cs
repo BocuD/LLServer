ff87c0b8de52eb55925ebb62b9828e70f20a79eb
BocuD <duco.born@gmail.com>
2023-12-29 02:31:32 +0100

    Add strings extensions for easier bitmask operations

---
+++ LLServer/Common/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace LLServer.Common;
+
+public static class StringFlagsExtensions
+{
+    public static string SetFlag(this string s, int index)
+    {
+        var charArray = s.ToCharArray();
+        charArray[index] = '1';
+        return new string(charArray);
+    }
+    
+    public static string ClearFlag(this string s, int index)
+    {
+        var charArray = s.ToCharArray();
+        charArray[index] = '0';
+        return new string(charArray);
+    }
+    
+    public static bool GetFlag(this string s, int index)
+    {
+        return s[index] == '1';
+    }
+    
+    public static string SetFlag(this string s, int index, bool value)
+    {
+        return value ? s.SetFlag(index) : s.ClearFlag(index);
+    }
+}
